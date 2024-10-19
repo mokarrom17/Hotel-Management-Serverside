bd4a72794765c60e5a43ad0d2f6511d5ef5aaa3b
mokarrom17 <mokarrombasher@gmail.com>
2024-10-19 22:43:57 +0600

    added findOne

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -27,11 +27,36 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const serviceCollection = client.db("HotelDb").collection("featured");
+    const serviceCollection = client.db("HotelDb").collection("features");
 
-    app.get("/featured", async (req, res) => {
+    app.get("/features", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/features/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: {
+          type: 1,
+          adults: 1,
+          child: 1,
+          price: 1,
+          image: 1,
+          gallery: 1,
+          description: 1,
+          cancellationPolicy: 1,
+          beds: 1,
+          amenities: 1,
+          checkInTime: 1,
+          checkOutTime: 1,
+          beds: 1,
+        },
+      };
+      const result = await serviceCollection.findOne(query, options);
       res.send(result);
     });
 
