4b9ef9ca45022fc388230f0d7d5cb19e4ce057ac
mokarrom17 <mokarrombasher@gmail.com>
2026-07-12 15:57:11 +0600

    middleware create and firebase key setup

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 require("dotenv").config();
+
 const port = process.env.PORT || 5000;
 
-//middleware
+// Firebase Admin SDK
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
+
+const serviceAccount = require("./hotel-management-firebase-adminsdk-.json");
+
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.HM_USER);
+// Firebase Token Verify Middleware
+
+const verifyFBToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  console.log("Header Token:", token);
+
+  if (!token) {
+    return res.status(401).send({
+      message: "Unauthorized access",
+    });
+  }
+
+  try {
+    const idToken = token.split(" ")[1];
+
+    const decoded = await getAuth().verifyIdToken(idToken);
+
+    req.decoded_email = decoded.email;
+
+    console.log("Verified User:", req.decoded_email);
+
+    next();
+  } catch (error) {
+    console.log(error);
+
+    return res.status(403).send({
+      message: "Forbidden access",
+    });
+  }
+};
 
 const uri = `mongodb+srv://${process.env.HM_USER}:${process.env.HM_PASS}@cluster0.olgdgso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,16 +66,39 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
     const userCollection = client.db("HotelDb").collection("users");
-    const serviceCollection = client.db("HotelDb").collection("features");
+
+    const serviceCollection = client.db("HotelDb").collection("roomTypes");
+
     const bookingCollection = client.db("HotelDb").collection("bookings");
-    app.get("/features", async (req, res) => {
-      const cursor = serviceCollection.find();
-      const result = await cursor.toArray();
+
+    // =========================
+    // Rooms
+    // =========================
+
+    app.get("/roomTypes", async (req, res) => {
+      const result = await serviceCollection.find().toArray();
+
       res.send(result);
     });
+
+    // Single Room
+
+    app.get("/roomTypes/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result = await serviceCollection.findOne({
+        _id: new ObjectId(id),
+      });
+
+      res.send(result);
+    });
+
+    // =========================
+    // Users
+    // =========================
 
     app.post("/users", async (req, res) => {
       try {
@@ -44,59 +109,77 @@ async function run() {
         });
 
         if (existingUser) {
-          return res.send({ message: "User already exists", insertOne: null });
+          return res.send({
+            message: "User already exists",
+          });
         }
+
         const result = await userCollection.insertOne(user);
+
         res.send(result);
       } catch (error) {
-        console.error("Error inserting user:", error);
-        res.status(500).send({ message: "Internal Server Error" });
+        res.status(500).send({
+          message: "Server Error",
+        });
       }
     });
 
-    app.get("/features/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const options = {
-        // Include only the `title` and `imdb` fields in the returned document
-        projection: {
-          type: 1,
-          adults: 1,
-          child: 1,
-          price: 1,
-          image: 1,
-          gallery: 1,
-          description: 1,
-          cancellationPolicy: 1,
-          beds: 1,
-          amenities: 1,
-          checkInTime: 1,
-          checkOutTime: 1,
-          beds: 1,
-        },
-      };
-      const result = await serviceCollection.findOne(query, options);
-      res.send(result);
-    });
+    // =========================
+    // Create Booking
+    // Protected
+    // =========================
 
-    // Bookings Information
-    app.post("/bookings", async (req, res) => {
+    app.post("/bookings", verifyFBToken, async (req, res) => {
       const booking = req.body;
-      console.log(booking);
+
+      console.log("Booking email:", booking.customerEmail);
+
+      console.log("Token email:", req.decoded_email);
+
+      if (booking.customerEmail !== req.decoded_email) {
+        return res.status(403).send({
+          message: "Forbidden",
+        });
+      }
+
       const result = await bookingCollection.insertOne(booking);
+
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
+    // =========================
+    // Get User Bookings
+    // Protected
+    // =========================
+
+    app.get("/bookings", verifyFBToken, async (req, res) => {
+      const email = req.query.email;
+
+      if (email !== req.decoded_email) {
+        return res.status(403).send({
+          message: "Forbidden access",
+        });
+      }
+
+      const result = await bookingCollection
+        .find({
+          customerEmail: email,
+        })
+        .toArray();
+
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
